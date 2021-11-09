9a6fe844fdaeabe9d2bb4b9a0912798705b9e9ee
AfonSir <afonsir17@gmail.com>
2021-11-09 00:32:49 -0300

    Chore: add I18n pt-BR

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,7 @@ module Iara
     #
     # config.time_zone = 'Central Time (US & Canada)'
     # config.eager_load_paths << Rails.root.join('extras')
+    config.i18n.default_locale = :'pt-BR'
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
