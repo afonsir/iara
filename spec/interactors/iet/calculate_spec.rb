7d7fd47f0c95b6e5c9b83ea3f96070ae046f2015
AfonSir <afonsir17@gmail.com>
2021-11-09 02:30:49 -0300

    Test: add interactor to calculate iet value (with pt and cl)

---
+++ spec/interactors/iet/calculate_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe IET::Calculate, type: :interactor do
+  subject(:interactor) { described_class.call(params) }
+
+  let(:params) do
+    {
+      cl_in_micrograms: cl_in_micrograms,
+      pt_in_micrograms: pt_in_micrograms,
+      type:             type
+    }
+  end
+
+  let(:cl_in_micrograms) { 10 }
+  let(:pt_in_micrograms) { 10 }
+
+  describe '.call' do
+    context 'when type is reservatorios' do
+      let(:type) { 'reservatorios' }
+
+      it { is_expected.to be_a_success }
+
+      it { expect(interactor.calculated_cl_value).to be_within(0.00005).of(58.02176) }
+      it { expect(interactor.calculated_pt_value).to be_within(0.00005).of(48.41639) }
+      it { expect(interactor.iet_value).to be_within(0.00005).of(53.21907) }
+    end
+
+    context 'when type is rios' do
+      let(:type) { 'rios' }
+
+      it { is_expected.to be_a_success }
+
+      it { expect(interactor.calculated_cl_value).to be_within(0.00005).of(70.03043) }
+      it { expect(interactor.calculated_pt_value).to be_within(0.00005).of(45.89962) }
+      it { expect(interactor.iet_value).to be_within(0.00005).of(57.96502) }
+    end
+  end
+end
