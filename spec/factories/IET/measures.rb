b8a2e439009eae78501116fa6ea7e93741e46fdd
AfonSir <afonsir17@gmail.com>
2021-11-09 00:03:52 -0300

    Test: add type column to measure model

--- spec/factories/IET/measures.rb
+++ spec/factories/IET/measures.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
   factory :measure, class: 'IET::Measure' do
     category { IET::Measure.categories.keys.sample }
     coords   { build(:geo_point) }
+    type     { IET::Measure.types.keys.sample }
     value    { Faker::Number.decimal(l_digits: 2) }
   end
 end
