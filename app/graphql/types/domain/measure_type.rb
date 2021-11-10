e7b6ad441fed712973378851ebcc9f49ba5ce199
AfonSir <afonsir17@gmail.com>
2021-11-10 00:44:03 -0300

    Feat: add measure graphql type

---
+++ app/graphql/types/domain/measure_type.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Types
+  module Domain
+    class MeasureType < Types::BaseObject
+      description I18n.t('graphql.types.domain.measure_type.description')
+
+      field :id, Int, null: false,
+        description: I18n.t('graphql.types.domain.measure_type.fields.id')
+
+      field :iet_category, Enums::CategoryType, null: false,
+        description: I18n.t('graphql.types.domain.measure_type.fields.iet_category')
+
+      field :iet_value, Float, null: false,
+        description: I18n.t('graphql.types.domain.measure_type.fields.iet_value')
+
+      field :location_type, Enums::LocationType, null: false,
+        description: I18n.t('graphql.types.domain.measure_type.fields.location_type')
+
+      field :created_at, GraphQL::Types::ISO8601DateTime, null: false,
+        description: I18n.t('graphql.types.domain.measure_type.fields.created_at')
+
+      field :updated_at, GraphQL::Types::ISO8601DateTime, null: false,
+        description: I18n.t('graphql.types.domain.measure_type.fields.updated_at')
+    end
+  end
+end
