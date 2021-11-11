4189ae3bd29702f89884e0469fd307af19f4ff75
AfonSir <afonsir17@gmail.com>
2021-11-11 18:41:25 -0300

    Refactor: add trait to measure factory

--- spec/factories/measures.rb
+++ spec/factories/measures.rb
@@ -4,7 +4,13 @@ FactoryBot.define do
   factory :measure, class: 'Measure' do
     coords        { build(:geo_point) }
     iet_category  { Measure.iet_categories.keys.sample }
-    iet_value     { Faker::Number.decimal(l_digits: 2) }
+    iet_value     { Faker::Number.decimal(l_digits: 2, r_digits: 5) }
     location_type { Measure.location_types.keys.sample }
+    created_at    { Time.zone.now }
+    updated_at    { Time.zone.now }
+
+    trait :coords_in_wkt do
+      coords { Geo.to_wkt(build(:geo_point)) }
+    end
   end
 end
