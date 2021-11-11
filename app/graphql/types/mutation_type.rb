0f11389666e9e83b9cc6f4c71bce856b1050feec
AfonSir <afonsir17@gmail.com>
2021-11-11 00:45:21 -0300

    Feat: add create iet measure mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :calculate_iet, mutation: Mutations::CalculateIet
+    field :create_iet_measure, mutation: Mutations::CreateIetMeasure
   end
 end
