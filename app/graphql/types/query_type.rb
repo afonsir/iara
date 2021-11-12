0cb53ab9718c8c74113677a0f954eeed4ee64978
AfonSir <afonsir17@gmail.com>
2021-11-12 00:29:35 -0300

    Feat: add query to search measures from a given point

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,5 +8,7 @@ module Types
 
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
+
+    field :fetch_near_measures, resolver: Queries::FetchNearMeasures
   end
 end
