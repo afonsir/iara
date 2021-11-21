57d2eeb74af2f195161b4a94d73f10faa780c6b9
AfonSir <afonsir17@gmail.com>
2021-11-21 16:22:33 -0300

    Refactor: improve coordinate validation

--- app/interactors/validators/coordinates.rb
+++ app/interactors/validators/coordinates.rb
@@ -8,19 +8,23 @@ module Validators
     delegate :latitude, :longitude, to: :context
 
     def call
-      return if latitude.present? && longitude.present?
+      return if valid_latitude? && valid_longitude?
 
       context.fail!(error: validation_message)
     end
 
     private
 
+    def valid_latitude?
+      latitude.present? && latitude.abs <= 90
+    end
+
+    def valid_longitude?
+      longitude.present? && longitude.abs <= 180
+    end
+
     def validation_message
-      I18n.t(
-        'errors.format',
-        attribute: I18n.t('activerecord.attributes.measure.coords'),
-        message:   I18n.t('errors.messages.blank')
-      )
+      I18n.t('errors.custom.invalid_coordinates')
     end
   end
 end
